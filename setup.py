8695126c705a2f1f0096eed82c873b484f31d0cc
Michael Cohen <mic@velocidex.com>
2020-02-08 04:09:26 +1000

    Added LICENSE

--- setup.py
+++ setup.py
@@ -9,10 +9,9 @@ from distutils.core import setup
 
 setup(
     name="pyvelociraptor",
-    version="0.1",
+    version="0.1.1",
     packages=['pyvelociraptor'],
     description="PyVelociraptor is the python binding for the Velociraptor API",
-    long_description=io.open("README.md", "rt", encoding='utf8').read(),
     license="GPL",
     url="https://github.com/Velocidex/pyvelociraptor",
     author="The Velociraptor Team",

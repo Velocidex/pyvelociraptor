b90ae2cbe4daa682f974add6578b2f057e985a3c
Mike Cohen <scudette@users.noreply.github.com>
2020-03-25 17:24:30 +1100

    Support decrypting the private key. (#3)
    In recent velociraptor versions you can now protect the private key
    with a password. This change allows the python binding to unlock the
    key automatically.

--- pyvelociraptor/__init__.py
+++ pyvelociraptor/__init__.py
@@ -1,7 +1,12 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+
+import getpass
 import os
 import yaml
 
-def LoadConfigFile(config_file=None):
+def LoadConfigFile(config_file=None, password=None, backend=default_backend()):
     if config_file is None:
         config_file = os.environ.get("VELOCIRAPTOR_API_FILE")
 
@@ -12,5 +17,23 @@ def LoadConfigFile(config_file=None):
         config = yaml.safe_load(open(config_file).read())
     except Exception as e:
         raise TypeError("Unable to parse config file from %s: %s" % (config_file, e))
+
+    while "ENCRYPTED" in config["client_private_key"]:
+        try:
+            password = getpass.getpass("Password: ").encode()
+            pem = config["client_private_key"].encode("utf8")
+
+            key = load_pem_private_key(pem, password=password, backend=backend)
+            config["client_private_key"]= key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption()
+            ).decode("utf8")
+
+            break
+
+        except (TypeError, ValueError) as e:
+            print("Error: %s" % e)
+            continue
 
     return config
